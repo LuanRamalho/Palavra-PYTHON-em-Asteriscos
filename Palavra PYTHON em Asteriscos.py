ea36e95487b2436a0ae384001b8117081d34b195
Luan Ramalho <32373552+LuanRamalho@users.noreply.github.com>
2024-12-25 13:31:49 -0300

    Add files via upload

---
+++ Palavra PYTHON em Asteriscos.py
@@ -0,0 +1,13 @@
+# Programa para imprimir a palavra PYTHON com asteriscos
+
+def imprimir_com_asteriscos():
+    # Cada letra da palavra "PYTHON" representada por asteriscos
+    print("*****  *   *  *****  *   *   ***   **    *")
+    print("*   *   * *     *    *   *  *   *  * *   *")
+    print("****     *      *    *****  *   *  *  *  *")
+    print("*        *      *    *   *  *   *  *   * *")
+    print("*        *      *    *   *   ***   *     *")
+
+# Chamada da função para exibir o resultado
+imprimir_com_asteriscos()
+input()
